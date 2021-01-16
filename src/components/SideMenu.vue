7cf07b8eb23538c29070977f0368ea3bbcd15fda
sssht03 <matsuo.shuta@arakawa-lab.com>
2021-01-17 05:17:40 +0900

    modify sidemenu

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-layout wrap>
+      <v-container fluid fixed>
+        <v-row
+          v-for="item in contensList"
+          :key="item"
+          justify="end"
+          class="pa-4"
+        >
+          <v-btn text>{{ item }}</v-btn>
+        </v-row>
+      </v-container>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    contensList: ["About", "Carrer", "Skills", "Product"],
+  }),
+};
+</script>
+
+<style></style>
